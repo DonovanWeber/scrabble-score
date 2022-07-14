fba3998275c5d3e5e81893d9eb4e0967557e34e9
Donovan Weber <donovanweber03@gmail.com>
2022-07-14 14:32:45 -0400

    3rd test finally passed returns the kvp
    Co-authored-by: Peter Vang <vang_peter06@yahoo.com>

--- Scrabble.Tests/Model.Tests/ScrabbleTests.cs
+++ Scrabble.Tests/Model.Tests/ScrabbleTests.cs
@@ -6,16 +6,37 @@ using ScrabbleScore.Models;
 namespace ScrabbleScore.Tests 
 {
   [TestClass]
-  public class ScrabbleTests
+  public class ScrabbleTests 
   {
     [TestMethod]
     public void ScrabbleConstructor_CreatesInstanceOfScrabble_Scrabble()
     {
-      Scrabble newScrabble = new Scrabble("test");
+      Scrabble newScrabble = new Scrabble("a", 1);
       Assert.AreEqual(typeof(Scrabble), newScrabble.GetType());
     }
-
-    
+    [TestMethod]
+    public void GetLetters_ReturnsLetters_String()
+    {
+      string letter = "a";
+      int valueOfLetter = 1;
+      Scrabble newScrabble = new Scrabble(letter, valueOfLetter);
+      string result = newScrabble.Letters;
+      Assert.AreEqual(result, letter);
+    }
+    [TestMethod]
+    public void GetValues_ReturnValues_int()
+    {
+      string letter = "a";
+      int valueOfLetter = 1;
+      Scrabble newScrabble = new Scrabble(letter, valueOfLetter);
+      Dictionary<string, int> newDictionaryScrabble = new Dictionary<string, int>();
+      newDictionaryScrabble.Add("a", 1);
+      int testScrabbleValue = newDictionaryScrabble["a"];
+      Dictionary<string, int> result = Scrabble.GetAll();
+      int finalResult = result["a"];
+     
+      Assert.AreEqual(testScrabbleValue, finalResult);
+    }
 
   }
 }
